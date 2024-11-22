df20f190265b93fb93dd29bec19d12273690f6e2
nikgrbn <Rmaps123@gmail.com>
2024-11-22 16:36:48 +0200

    NP-15: ICommand interface init

---
+++ inc/ICommand.h
@@ -0,0 +1,15 @@
+//
+// Created by Nikita on 11/22/2024.
+//
+
+#ifndef NETFLIX_PROJECT_ICOMMAND_H
+#define NETFLIX_PROJECT_ICOMMAND_H
+
+
+class ICommand {
+public:
+    virtual void execute() = 0;
+};
+
+
+#endif //NETFLIX_PROJECT_ICOMMAND_H
---
+++ src/ICommand.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Nikita on 11/22/2024.
+//
+
+#include "../inc/ICommand.h"
