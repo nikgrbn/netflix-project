1a3a91dd60fa2751706ff1fb0a36b07a81b82468
nikgrbn <Rmaps123@gmail.com>
2024-11-24 18:33:41 +0200

    NP-22: created a core structure

---
+++ tests/AddCommandTest.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+
+// A simple test case
+TEST(AddCommandTest, CheckEquality) {
+  // TODO: Add your test code here
+  // For example:
+  EXPECT_EQ(1, 1); // Test if 1 equals 1
+}
