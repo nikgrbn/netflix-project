e00376ced591b313529fa9810f21fe279d7d9670
nikgrbn <Rmaps123@gmail.com>
2024-11-26 00:11:40 +0200

    NP-30: info method and size check

--- src/AddCommand.cpp
+++ src/AddCommand.cpp
@@ -5,10 +5,12 @@
 #include "../inc/AddCommand.h"
 
 void AddCommand::execute(vector<string> commands) {
+    if (commands.size() <= 2) { // Check if at least 3 arguments are provided
+        throw invalid_argument("Invalid number of arguments");
+    }
     // TODO: Implement the execute method
 }
 
 string AddCommand::info() {
-    // TODO: Implement the info method
-    return "AddCommand information";
+    return "add [userid] [movieid1] [movieid2] …";
 }
