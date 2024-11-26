fa63481dffa6eade15e5a7f1b844a1f239a0993a
nikgrbn <Rmaps123@gmail.com>
2024-11-26 09:22:25 +0200

    NP-24: static data variable for re-usage

--- tests/AddCommandTest.cpp
+++ tests/AddCommandTest.cpp
@@ -3,6 +3,7 @@
 #include <gtest/gtest.h>
 #include <vector>
 #include "../inc/AddCommand.h"
+#include "TestVars.cpp"
 
 using namespace std;
 
@@ -47,22 +48,8 @@ TEST(AddCommandTest, CheckCorrectDataAddition) {
   ofstream clearFile(testFilePath, ofstream::trunc); // Clear the file
   clearFile.close();
 
-  // Commands and expected lines
-  vector<pair<vector<string>, string>> testCases = {
-    {{"add", "1", "100", "101", "102", "103"}, "1 100 101 102 103"},
-    {{"add", "2", "101", "102", "104", "105", "106"}, "2 101 102 104 105 106"},
-    {{"add", "3", "100", "104", "105", "107", "108"}, "3 100 104 105 107 108"},
-    {{"add", "4", "100", "105", "106", "107", "109", "110"}, "4 100 105 106 107 109 110"},
-    {{"add", "5", "100", "103", "104", "108", "111"}, "5 100 103 104 108 111"},
-    {{"add", "6", "100", "103", "104", "110", "111", "112", "113"}, "6 100 103 104 110 111 112 113"},
-    {{"add", "7", "102", "105", "106", "107", "108", "109", "110"}, "7 102 105 106 107 108 109 110"},
-    {{"add", "8", "101", "104", "105", "106", "109", "111", "114"}, "8 101 104 105 106 109 111 114"},
-    {{"add", "9", "100", "103", "105", "107", "112", "113", "115"}, "9 100 103 105 107 112 113 115"},
-    {{"add", "10", "100", "102", "105", "106", "107", "109", "110", "116"}, "10 100 102 105 106 107 109 110 116"}
-  };
-
   // Run through each test case in the vector
-  for (const auto& testCase : testCases) {
+  for (const auto& testCase : TestVars::testData) {
     const vector<string>& command = testCase.first;
     const string& expectedLine = testCase.second;
 
---
+++ tests/TestVars.h
@@ -0,0 +1,18 @@
+//
+// Created by nikita on 11/26/24.
+//
+
+#ifndef TESTS_H
+#define TESTS_H
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class TestVars {
+public:
+    static const vector<pair<vector<string>, string>> testData;
+};
+
+#endif //TESTS_H
