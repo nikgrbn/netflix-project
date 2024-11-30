a20c00ff34e40bc3034a4f8f33aca365d9c779ce
nikgrbn <Rmaps123@gmail.com>
2024-11-30 18:09:30 +0200

    NP-39: debug method removal

--- src/HelloWorldCommand.cpp
+++
@@ -1,9 +0,0 @@
-#include "../inc/HelloWorldCommand.h"
-
-string HelloWorldCommand::execute(vector<string> commands) {
-    return "Hello, World!";
-}
-
-string HelloWorldCommand::info() {
-    return "Prints 'Hello, World!'";
-}
--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,7 @@
 #include "../inc/RecommendCommand.h"
 #include "../inc/App.h"
 #include "../inc/ConsoleMenu.h"
-#include "../inc/HelloWorldCommand.h"
 #include "../inc/LocalDataManager.h"
-
 #include <iostream>
 #include <map>
 
@@ -16,10 +14,6 @@ int main() {
 
     // Create a map of commands
     map<string, ICommand*> commands;
-
-    // TODO: Remove
-    ICommand* helloWorldCommand = new HelloWorldCommand();
-    commands["saying-hi"] = helloWorldCommand;
 
     ICommand* addCommand = new AddCommand(dataManager);
     commands["add"] = addCommand;
@@ -33,7 +27,6 @@ int main() {
     App app(menu, commands); // Create an instance of App
     app.run(); // Start the application
 
-    delete helloWorldCommand; // Free the memory
     delete menu; // Free the memory
 
     return 0; // Exit the application
