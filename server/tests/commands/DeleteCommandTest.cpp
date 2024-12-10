23ba7d25c4eefc5d298fb6ac70f1588ae33a7fb8
nikgrbn <Rmaps123@gmail.com>
2024-12-10 21:01:59 +0200

    NP-55: tests for delete command implemented

--- server/tests/commands/DeleteCommandTest.cpp
+++ server/tests/commands/DeleteCommandTest.cpp
@@ -1,6 +1,58 @@
 #include <gtest/gtest.h>
-#include "../inc/commands/DeleteCommand.h"
-#include "../inc/data_manager/LocalDataManager.h"
+#include "commands/DeleteCommand.h"
+#include "data_manager/LocalDataManager.h"
+#include "../utils/TestUtils.h"
+
+// Delete data from the file and check it
+TEST(DeleteCommandTest, CheckCorrectDataDeletion) {
+    IDataManager* dataManager = new LocalDataManager();
+    DeleteCommand deleteCommand(dataManager);
+
+    // Ensure the test file is clean and ready for writing
+    string testFilePath = "data/users.txt";
+    ofstream outFile(testFilePath, ofstream::trunc); // Open in trunc mode to clear content
+
+    // Fill file with data
+    for (const auto& dataLine : TestUtils::testData) {
+        outFile << dataLine.second << endl; // Write data to a file
+    }
+    outFile.close();
+
+    // Run through each test case in the vector
+    for (const auto& testCase : TestUtils::modifiedData) {
+        const vector<string>& command = testCase.first;
+        const string& expectedLine = testCase.second;
+
+        // Act
+        string res = deleteCommand.execute(command);
+        EXPECT_EQ(res, StatusCodes::NO_CONTENT);
+
+        // Assert
+        vector<string> fileLines = TestUtils::readFileLines(testFilePath);     // Read file
+        auto it = find(fileLines.begin(), fileLines.end(), expectedLine);
+        EXPECT_NE(it, fileLines.end());
+    }
+
+    // Try to delete a non-existing movie
+    EXPECT_EQ(deleteCommand.execute(vector<string>{"DELETE", "1", "999"}), StatusCodes::NOT_FOUND);
+}
+
+// Check illegal arguments
+TEST(DeleteCommandTest, CheckIllegalArguments) {
+    IDataManager* dataManager = new LocalDataManager();
+
+    DeleteCommand* m = new DeleteCommand(dataManager);
+
+    // Zero arguments are illegal
+    EXPECT_THROW(m->execute(vector<string>{}), StatusCodeException);
+
+    // One argument is illegal
+    EXPECT_THROW(m->execute(vector<string>{"DELETE"}), StatusCodeException);
+
+    // Two arguments are also illegal
+    EXPECT_THROW(m->execute(vector<string>{"DELETE 1"}), StatusCodeException);
+}
+
 
 // Check info method
 TEST(DeleteCommandTest, CheckInfo) {
