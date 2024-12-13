6ee43fd85f3f642dfb980272ea15c1ed320219ad
michalsagiv <michalsagiv3@gmail.com>
2024-12-13 09:43:05 +0200

    NP-52: more test implementations

--- server/tests/commands/PatchCommandTest.cpp
+++ server/tests/commands/PatchCommandTest.cpp
@@ -6,8 +6,6 @@
 
 using namespace std;
 
-
-
 // Use add command to add data to the file and check it
 TEST(PatchCommandTest, CheckCorrectDataAddition) {
     IDataManager* dataManager = new LocalDataManager();
@@ -34,23 +32,24 @@ TEST(PatchCommandTest, CheckCorrectDataAddition) {
     }
 }
 
-// // Check illegal arguments
-// TEST(AddCommandTest, CheckIllegalArguments) {
-//   AddCommand* m = new AddCommand(dataManager);
+// Check illegal arguments
+TEST(PatchCommandTest, CheckIllegalArguments) {
+  IDataManager* dataManager = new LocalDataManager();
+  PatchCommand* m = new PatchCommand(dataManager);
 
-//   // Zero arguments are illegal
-//   EXPECT_THROW(m->execute(vector<string>{}), StatusCodeException);
+  // Zero arguments are illegal
+  EXPECT_THROW(m->execute(vector<string>{}), StatusCodeException);
 
-//   // One argument is illegal
-//   EXPECT_THROW(m->execute(vector<string>{"POST"}), StatusCodeException);
+  // One argument is illegal
+  EXPECT_THROW(m->execute(vector<string>{"PATCH"}), StatusCodeException);
 
-//   // Two arguments are also illegal
-//   EXPECT_THROW(m->execute(vector<string>{"POST 12"}), StatusCodeException);
-// }
+  // Two arguments are also illegal
+  EXPECT_THROW(m->execute(vector<string>{"PATCH 12"}), StatusCodeException);
+}
 
-// Check info method
-// TEST(AddCommandTest, CheckInfo) {
-  // EXPECT_EQ(AddCommand(dataManager).info(), "POST, arguments: [userid] [movieid1] [movieid2] …");
-// }
+TEST(PatchCommandTest, CheckInfo) {
+  IDataManager* dataManager = new LocalDataManager();
+  EXPECT_EQ(PatchCommand(dataManager).info(), "PATCH, arguments: [userid] [movieid1] [movieid2] …");
+}
 
 
