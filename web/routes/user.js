b86c010c0b237fcadab7f35473aad9892b05a884
nikgrbn <Rmaps123@gmail.com>
2024-12-25 21:16:45 +0200

    NP-82: Router refactor

--- web/routes/user.js
+++ web/routes/user.js
@@ -10,15 +10,4 @@ router.route('/')
 router.route('/:id')
     .get(userController.getUserById);
 
-// Middleware to ensure User-ID header exists
-const ensureUserHeader = (req, res, next) => {
-    const userId = req.headers['user-id'];
-    if (!userId) {
-        return res.status(401).json({ error: error.ID_HEADER_REQUIRED });
-    }
-    req.userId = userId; // Attach userId to the request object for downstream use
-    next();
-};
-router.use(ensureUserHeader); // Apply this middleware to all routes below
-
 module.exports = router;
