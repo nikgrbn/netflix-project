cc9b69f385d3fc71c49a91b1cfbab8d9c24183b9
michalsagiv <michalsagiv3@gmail.com>
2025-01-16 15:13:04 +0200

    NP-112: Basic implementation

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignUpPage from './pages/SignUpPage';
 import LandingPage from './pages/LandingPage';
 import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/home" element={<HomePage />} />
         <Route path="/" element={<LandingPage />} />
+        <Route path="/signin" element={<LoginPage />} />
       </Routes>
     </Router>
   );
