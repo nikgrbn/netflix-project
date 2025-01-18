9fcaab810bc395816bbcfef3757f1415022cbc57
nikgrbn <Rmaps123@gmail.com>
2025-01-18 11:13:43 +0200

    NP-113: Back to Sign-In link added

--- react-client/src/pages/SignUpPage.js
+++ react-client/src/pages/SignUpPage.js
@@ -1,8 +1,9 @@
 import React from "react";
 import SignUpForm from "../components/SignUp/SignUpForm";
 import "./SignUpPage.css"; // Ensure this import is correct
-import { signUpUser } from '../services/api'; // Import API service
+import { signUpUser } from "../services/api"; // Import API service
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const SignUpPage = () => {
   const navigate = useNavigate();
@@ -12,7 +13,6 @@ const SignUpPage = () => {
       await signUpUser(data);
       alert("User signed up successfully!");
       navigate("/signin");
-
     } catch (error) {
       console.error("Error signing up", error);
     }
@@ -22,6 +22,14 @@ const SignUpPage = () => {
     <div className="signup-page">
       <div className="signup-content">
         <SignUpForm onSubmit={handleFormSubmit} />
+
+        <div className="signup-footer">
+          <Link to="/signin" className="signup-footer-link">
+            <span className="white-text">Already have an account?</span>{" "}
+            <span className="blue-text">Sign in</span>
+          </Link>
+        </div>
+
       </div>
     </div>
   );
