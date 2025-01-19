590a4be72944e2b8ef6c834cb3b7f676f3343c20
nikgrbn <Rmaps123@gmail.com>
2025-01-19 21:35:26 +0200

    NP-114: User-ID Header added

--- web/routes/movie.js
+++ web/routes/movie.js
@@ -27,6 +27,11 @@ const ensureUserHeader = (req, res, next) => {
   if (!req.jwtContent) {
     return res.status(401).json({ error: errors.TOKEN_NOT_VALID });
   }
+
+  // Attach user ID to the request if header present
+  if (req.headers["user-id"]) {
+    req.userId = req.headers["user-id"];
+  }
   next();
 };
 router.use(ensureUserHeader); // Apply this middleware to all routes below
