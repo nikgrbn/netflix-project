cd18f3b5b0ae0a5567de07d275566666cff67f3a
nikgrbn <Rmaps123@gmail.com>
2025-01-19 23:42:08 +0200

    NP-114: Smooth Header color transition from transparent to opaque

--- react-client/src/components/Home/HomeHeader.js
+++ react-client/src/components/Home/HomeHeader.js
@@ -1,19 +1,38 @@
-import React from "react";
+
 import HomeSearchBar from "./HomeSearchBar";
 import DarkMode from "./DarkMode/DarkMode";
+import React, { useState, useEffect } from 'react';
+
 
 const HomeHeader = ({ username, profilePicture, onSearch }) => {
+  const [opacity, setOpacity] = useState(0);
+  
+  useEffect(() => {
+    const handleScroll = () => {
+      // Calculate opacity based on scroll position
+      // We'll make it fully opaque by 200px of scroll
+      const scrolled = Math.min(window.scrollY / 150, 1);
+      setOpacity(scrolled);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const headerStyle = {
+    backgroundColor: `rgba(var(--background-secondary-color-rgb), ${opacity})`,
+    boxShadow: opacity > 0 ? `0 2px 5px rgba(var(--background-secondary-color-rgb), ${opacity})` : 'none'
+  };
+
   return (
-    <header className="home-header">
+    <header className="home-header" style={headerStyle}>
       <div className="home-header-left">
-        <img
-          src="netflix-logo.png"
-          alt="Netflix Logo"
-          className="netflix-logo"
-        />
+        <img src="netflix-logo.png" alt="Netflix Logo" className="netflix-logo" />
         <HomeSearchBar onSearch={onSearch} />
       </div>
-
       <div className="home-header-right">
         <span className="username">{username}</span>
         <img src={profilePicture} alt="Profile" className="profile-picture" />
@@ -22,5 +41,6 @@ const HomeHeader = ({ username, profilePicture, onSearch }) => {
     </header>
   );
 };
+
 
 export default HomeHeader;
