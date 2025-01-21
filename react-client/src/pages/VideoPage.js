b841edb150d5f6ccc4c269d0530e09362c36da83
nikgrbn <Rmaps123@gmail.com>
2025-01-21 10:44:35 +0200

    NP-114: Video page initialized

---
+++ react-client/src/pages/VideoPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./VideoPage.css";
+
+const VideoPage = () => {
+    return (
+        <div className='video-page'>
+            <h1>Video Page</h1>
+            <p>Welcome to the video page!</p>
+        </div>
+    );
+};
+
+export default VideoPage;
