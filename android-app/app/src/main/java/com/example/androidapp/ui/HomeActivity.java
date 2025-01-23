f1e852264b05de47fd05a80eb901d1b8dc4b529f
nikgrbn <Rmaps123@gmail.com>
2025-01-23 20:40:24 +0200

    NP-126: Home Activity initilizaed

--- android-app/app/src/main/java/com/example/androidapp/ui/HomeActivity.java
+++ android-app/app/src/main/java/com/example/androidapp/ui/HomeActivity.java
@@ -1,7 +1,6 @@
 package com.example.androidapp.ui;
 
 import android.os.Bundle;
-import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,33 +9,18 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.androidapp.R;
-import com.google.android.material.button.MaterialButton;
 
-public class SignInActivity extends AppCompatActivity {
-
-    private EditText nameInput;
-    private EditText passwordInput;
-    private MaterialButton signInButton;
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_sign_in);
+        setContentView(R.layout.activity_home);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        // Back arrow button
-        findViewById(R.id.btnBackArrow).setOnClickListener(v -> {
-            finish();
-        });
-
-        // Form fields
-        nameInput = findViewById(R.id.etName);
-        passwordInput = findViewById(R.id.etPassword);
-        signInButton = findViewById(R.id.btnSignIn);
     }
 }
