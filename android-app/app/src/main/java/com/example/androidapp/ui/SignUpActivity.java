4cf9f9b31f63171110d8d3114b84dc3cd210b346
nikgrbn <Rmaps123@gmail.com>
2025-01-23 17:50:53 +0200

    NP-126: UI SignIn/Up pages initialized

--- android-app/app/src/main/java/com/example/androidapp/ui/SignUpActivity.java
+++ android-app/app/src/main/java/com/example/androidapp/ui/SignUpActivity.java
@@ -1,6 +1,5 @@
 package com.example.androidapp.ui;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -11,27 +10,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.androidapp.R;
 
-public class LandingActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_landing);
+        setContentView(R.layout.activity_sign_up);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        // Set up the sign in button
-        findViewById(R.id.btnSignIn).setOnClickListener(v -> {
-            startActivity(new Intent(this, SignInActivity.class));
-        });
-
-        // Set up the sign up button
-        findViewById(R.id.btnSignUp).setOnClickListener(v -> {
-            startActivity(new Intent(this, SignUpActivity.class));
+        findViewById(R.id.btnBackArrow).setOnClickListener(v -> {
+            finish();
         });
     }
 }
