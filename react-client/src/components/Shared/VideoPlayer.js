afa81a0b729142ed6c0099f0a3425945e427ef98
nikgrbn <Rmaps123@gmail.com>
2025-01-23 21:18:31 +0200

    NP-122: Video player sttutering fixed

--- react-client/src/components/Shared/VideoPlayer.js
+++ react-client/src/components/Shared/VideoPlayer.js
@@ -6,6 +6,10 @@ const VideoPlayer = ({ movieId, controlsMode = false, isMuted = true, className 
   const API_BASE_URL = "http://localhost:19844/api";
   const videoUrl = `${API_BASE_URL}/movies/${movieId}/video`;
 
+  const handleCanPlay = (e) => {
+    e.target.play();
+  };
+
   const handleVideoError = () => {
     setError("Failed to load the video. Please try again later.");
   };
@@ -25,8 +29,12 @@ const VideoPlayer = ({ movieId, controlsMode = false, isMuted = true, className 
         controls={controlsMode}
         autoPlay
         playsInline
+        preload="auto"
+        muted={isMuted}
+        onCanPlay={handleCanPlay}
         onError={handleVideoError}
-        muted = {isMuted}
+        onPause={handleCanPlay}
+        onPlaying={() => console.log("Video is playing")}
       >
         <source src={videoUrl} type="video/mp4" />
         Your browser does not support the video tag.
